531a9bec02abb5f238f9524e940a1c8c65a8bf1c
arjun8009 <117773435+arjun8009@users.noreply.github.com>
2026-08-19 09:29:15 +0100

    Reset API keys in set_api_keys function
    Clear the API keys in the environment variables for security.

--- utils/keys.py
+++ utils/keys.py
@@ -2,6 +2,6 @@ import os
 
 # Function to set api keys. We want all api keys in one place. Useful when we want to remove them 
 def set_api_keys():
-    os.environ["OPENAI_API_KEY"] = "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"
-    os.environ["OSNDG_API_KEY"] = "mquJppFlzV2SgEFGgrbPV1mGZeD8u33b"
+    os.environ["OPENAI_API_KEY"] = ""
+    os.environ["OSNDG_API_KEY"] = ""
     print("Openai and ngd key set successfully")
